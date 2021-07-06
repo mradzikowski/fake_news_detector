5f222e46cbc2d1e18731c42d8aa113bfc011c8dd
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-07-06 10:48:15 +0200

    Initialize db with sqlalchemy and create news model

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 import os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -9,6 +11,22 @@ api = Api(app)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+
+class News(db.Model):
+    __tablename__ = "news"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(128), nullable=False)
+    broadcaster = db.Column(db.String(128), nullable=True)
+    url = db.Column(db.String(128), nullable=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    credibility = db.Column(db.Float, default=0, nullable=False)
+
+    def __init__(self, title, credibility):
+        self.title = title
+        self.credibility = credibility
 
 
 class Ping(Resource):
