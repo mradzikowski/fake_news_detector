af8710832e2e50a183d1a58ecaa328c98ada1e78
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-07-06 12:21:06 +0200

    Update to work with the app and db context directly

--- manage.py
+++ manage.py
@@ -1,8 +1,12 @@
+import sys
+
 from flask.cli import FlaskGroup
 
-from src import app, db
+from src import create_app, db
+from src.api.models import News
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command('recreate_db')
