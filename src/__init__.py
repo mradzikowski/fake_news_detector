1ffb5e94abf4f7cd152fbc37a23b17c7e9b066da
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-07-06 12:22:50 +0200

    Add method for creating app and intializing with the db

--- src/__init__.py
+++ src/__init__.py
@@ -5,36 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
-app = Flask(__name__)
 
-api = Api(app)
+db = SQLAlchemy()
 
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+def create_app(script_info=None):
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    db.init_app(app)
 
-class News(db.Model):
-    __tablename__ = "news"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(128), nullable=False)
-    broadcaster = db.Column(db.String(128), nullable=True)
-    url = db.Column(db.String(128), nullable=True)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
-    credibility = db.Column(db.Float, default=0, nullable=False)
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
 
-    def __init__(self, title, credibility):
-        self.title = title
-        self.credibility = credibility
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
 
-
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
-
-
-api.add_resource(Ping, '/ping')
+    return app
