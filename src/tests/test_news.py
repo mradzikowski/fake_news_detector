7598c28eda8044c4b242f4799847e51808aede69
Mateusz Radzikowski <radzik.radzikowski18@gmail.com>
2021-07-06 16:14:32 +0200

    Add tests for put news method

--- src/tests/test_news.py
+++ src/tests/test_news.py
@@ -94,9 +94,9 @@ def test_remove_news(test_app, test_database, add_news):
     resp_two = client.delete(f"news/{news.id}")
     data = json.loads(resp_two.data.decode())
     assert resp_two.status_code == 200
-    assert 'Coronavirus was removed!' in data['message']
+    assert "Coronavirus was removed!" in data["message"]
 
-    resp_three = client.get('/news')
+    resp_three = client.get("/news")
     data = json.loads(resp_three.data.decode())
     assert resp_three.status_code == 200
     assert len(data) == 0
@@ -104,7 +104,63 @@ def test_remove_news(test_app, test_database, add_news):
 
 def test_remove_news_incorrect_id(test_app, test_database):
     client = test_app.test_client()
-    resp = client.delete('/news/999')
+    resp = client.delete("/news/999")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert "News 999 does not exist" in data["message"]
+
+
+def test_update_news(test_app, test_database, add_news):
+    news = add_news(title="Coronavirus", url="coronavirus.com", credibility=9.0)
+    client = test_app.test_client()
+    resp_one = client.put(
+        f"/news/{news.id}",
+        data=json.dumps({"title": "me", "url": "me.com", "credibility": 10}),
+        content_type="application/json",
+    )
+    data = json.loads(resp_one.data.decode())
+    assert resp_one.status_code == 200
+    assert f"{news.id} was updated!" in data["message"]
+
+    resp_two = client.get(f"/news/{news.id}")
+    data = json.loads(resp_two.data.decode())
+    assert resp_two.status_code == 200
+    assert "me" in data["title"]
+    assert "me.com" in data["url"]
+    assert 10 == data["credibility"]
+
+
+def test_update_news_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.put(
+        "/news/1",
+        data=json.dumps({}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_update_news_invalid_keys(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.put(
+        "/news/1",
+        data=json.dumps({"title": "Corona"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_update_news_does_not_exist(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.put(
+        "/news/999",
+        data=json.dumps({"title": "me", "url": "me.com", "credibility": 10}),
+        content_type="application/json",
+    )
     data = json.loads(resp.data.decode())
     assert resp.status_code == 404
     assert "News 999 does not exist" in data["message"]
